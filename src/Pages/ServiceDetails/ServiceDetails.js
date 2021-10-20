be51c2186790eebafe29af203b9666d099e6a47e
Sheikhasib <sheikhhasib037@gmail.com>
2021-10-20 13:12:20 +0600

    serDetails done

--- src/Pages/ServiceDetails/ServiceDetails.js
+++ src/Pages/ServiceDetails/ServiceDetails.js
@@ -11,15 +11,14 @@ const ServiceDetails = () => {
     }, [])
     // console.log(data);
 
-    const serviceItem = data.filter( td => td.id === serviceId);
+    const serviceItem = data.find( td => td.id === serviceId);
     console.log(serviceItem);
 
     return (
         <div>
-            <h2>Our Service Details Page</h2>
-            <h3>Image: {serviceItem[0]?.img}</h3>
-            <h3>Service Name: {serviceItem[0]?.name}</h3>
-            <h3>Description: {serviceItem[0]?.description}</h3>
+            <h2 className="mb-5 text-warning">Our Service Details Page</h2>
+            <h3 className="mb-3">Service Name: {serviceItem?.name}</h3>
+            <h3>Description: {serviceItem?.description}</h3>
         </div>
     );
 };
